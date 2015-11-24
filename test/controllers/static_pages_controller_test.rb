171ef75552727614ebb47a6ebea815a09b614821
sunzhe <davidsunsz@hotmail.com>
2015-11-24 23:11:51 +0800

    update

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About #{@base_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
 end
