e03e4c43e07b1c80eb90dedc26377f89e7bf02fe
sunzhe <davidsunsz@hotmail.com>
2015-11-24 14:15:26 +0800

    add static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | Rails Blog"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | Rails Blog"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Rails Blog"
+  end
+
+end
