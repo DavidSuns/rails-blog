4aa8dbd8b666cbd56ab4a8012ae7a5b6c5a26426
sunzhe <davidsunsz@hotmail.com>
2015-11-25 20:15:01 +0800

    update

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://ruby.tabao.org'
+source 'https://ruby.taobao.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -25,6 +25,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+
+gem 'bcrypt',  '3.1.10'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
 
   get 'contact' => 'static_pages#contact'
+
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
