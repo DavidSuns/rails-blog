68bb283cb1b48a794ec8457c0bb3793b51f6f5ee
sunzhe <davidsunsz@hotmail.com>
2015-11-30 20:43:02 +0800

    add delete action

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,11 @@ module SessionsHelper
   end
 
   def logged_in?
-    !current_user.nil? 
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
