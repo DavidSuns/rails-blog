76a33d4dad6222a8f83f777be8ab3c8616ac738e
sunzhe <davidsunsz@hotmail.com>
2015-12-07 20:08:58 +0800

    email activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,14 +31,27 @@ class User < ActiveRecord::Base
   end
 
   # 如果指定的令牌和摘要匹配,返回 true
-  def authenticated?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
     return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  # 激活账户
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # 发送激活邮件
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now 
+  end
+
 
   private
 
