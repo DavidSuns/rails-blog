5ef296505b35b6d07fd275b87a6698fbf0fa5df8
sunzhe <davidsunsz@hotmail.com>
2015-12-11 17:30:48 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -73,8 +83,25 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ?", id) 
+    Micropost.where("user_id = ?", id)
   end
+
+  # 关注另一个用户
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+
+  # 取消关注另一个用户
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 如果当前用户关注了指定的用户,返回 true
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
 
   private
