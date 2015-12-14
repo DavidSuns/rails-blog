07ea6d4bd7cfd81e8808455f18ac5f66318ae327
sunzhe <davidsunsz@hotmail.com>
2015-12-14 13:57:35 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,7 +83,9 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                         WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # 关注另一个用户
